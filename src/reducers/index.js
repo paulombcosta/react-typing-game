bf2c11e30c92c6ef4bae3d345f519db6b72c7340
Paulo Costa <paulombcosta@gmail.com>
2017-07-02 00:59:12 -0300

    Removing unecessary reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 import WordsReducer from './reducer_words';
-import TypedWordsReducer from './reducer_typed_words';
 
 const rootReducer = combineReducers({
-    words: WordsReducer, typed_words: TypedWordsReducer
+    words: WordsReducer
 });
 
 export default rootReducer;
